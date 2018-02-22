17d92d8341bb7e87c09d0be478439184631f182e
Bigoud <bigoud33@gmail.com>
2018-02-22 19:42:52 +0100

    add modifications

---
+++ app/src/main/java/bigoud/com/dronepilot/controller/SDK/model/ViewWrapper.java
@@ -0,0 +1,22 @@
+package bigoud.com.dronepilot.controller.SDK.model;
+
+import android.view.View;
+
+public class ViewWrapper {
+
+    private int titleId;
+    private View view;
+
+    public ViewWrapper(View layoutView, int titleId) {
+        view = layoutView;
+        this.titleId = titleId;
+    }
+
+    public int getTitleId() {
+        return titleId;
+    }
+
+    public View getView() {
+        return view;
+    }
+}
