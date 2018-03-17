0dd311b1ce308370560a49098411747a83f94a7d
Aeres-Blade <aeresblade@gmail.com>
2018-03-17 13:48:08 +0100

    Added FeedView to PilotActivity and set up logic

--- app/src/main/java/bigoud/com/dronepilot/view/PilotActivity.java
+++ app/src/main/java/bigoud/com/dronepilot/view/PilotActivity.java
@@ -2,7 +2,7 @@ package bigoud.com.dronepilot.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -11,6 +11,7 @@ import bigoud.com.dronepilot.controller.DroneTask;
 import bigoud.com.dronepilot.controller.SimulationDrone;
 import bigoud.com.dronepilot.controller.VirtualDrone;
 import bigoud.com.dronepilot.model.Position;
+import dji.sdk.camera.VideoFeeder;
 
 public class PilotActivity extends AppCompatActivity
 {
@@ -27,11 +28,78 @@ public class PilotActivity extends AppCompatActivity
         DroneTask connect = drone.connect();
         connect.join();
 
+        VideoFeeder.VideoFeed feed = drone.getVideo();
+        if(feed != null)
+        {
+            ((VideoFeedView)findViewById(R.id.video_feed)).registerLiveVideo(feed, true);
+        }
+
+        if(!this.shoot())
+        {
+
+        }
+        else
+        {
+
+        }
     }
 
-    private void log(String message)
+    private boolean shoot()
     {
-        EditText logger = findViewById(R.id.console);
-        logger.append(message + "\n");
+        // Calculates center point;
+        Position middle = new Position();
+
+        DroneTask initFlight = drone.initFlight();
+        initFlight.join();
+
+        if(!initFlight.isSuccess())
+        {
+            Log.e("DronePilot", initFlight.getMessage());
+            return false;
+        }
+
+        for(Position pos : this.positions)
+        {
+            DroneTask moveTask = drone.moveTo(pos);
+            moveTask.join();
+
+            if(!moveTask.isSuccess())
+            {
+                Log.e("DronePilot", moveTask.getMessage());
+                drone.returnHome();
+                return false;
+            }
+
+            DroneTask lookTask = drone.lookAt(middle, false);
+            lookTask.join();
+
+            if(!lookTask.isSuccess())
+            {
+                Log.e("DronePilot", lookTask.getMessage());
+                drone.returnHome();
+                return false;
+            }
+
+            DroneTask photoTask = drone.takePhoto();
+            photoTask.join();
+
+            if(!photoTask.isSuccess())
+            {
+                Log.e("DronePilot", photoTask.getMessage());
+                drone.returnHome();
+                return false;
+            }
+        }
+
+        DroneTask homeTask = drone.returnHome();
+        homeTask.join();
+
+        if(!homeTask.isSuccess())
+        {
+            Log.e("DronePilot", homeTask.getMessage());
+            return false;
+        }
+
+        return true;
     }
 }
